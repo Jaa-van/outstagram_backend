40c7a528d816d9f1c5299715fa981239a16e0577
Jaa-van <127918643+Jaa-van@users.noreply.github.com>
2023-05-15 12:39:24 +0900

    Fix: Update comments router

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const postsRouter = require("./posts.route.js");
 const authRouter = require("./auth.route.js");
+const commentsRouter = require("./comments.route.js");
 
-router.use("/", [authRouter, postsRouter]);
+router.use("/", [authRouter, postsRouter, commentsRouter]);
 
 module.exports = router;
