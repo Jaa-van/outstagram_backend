c20816119d93b9449dd452f5ca9b5c2132fc19c7
Jaa-van <127918643+Jaa-van@users.noreply.github.com>
2023-05-15 18:48:54 +0900

    Feat: Fix cors error

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+const http = require("http");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 require("express-async-errors");
 const app = express();
+const cors = require("cors");
 const port = 3000;
+
+app.use(cors());
 
 const router = require("./routes");
 
