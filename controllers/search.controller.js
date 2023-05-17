5e6ea670171038dd1fb62413b0a71584e990b1eb
Jaa-van <ubuntu@ip-172-31-45-106.ap-northeast-2.compute.internal>
2023-05-17 16:10:45 +0000

    Fix: Update req.body to req.query at search

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -6,14 +6,12 @@ class SearchController {
   //게시물 생성
   searchUsersAndPosts = async (req, res, next) => {
     try {
-      const { search } = req.body;
+      const { search } = req.query;
       if (!search || search === "" || typeof search !== "string") {
         throw new Error("419/검색어의 형식이 올바르지 않습니다.");
       }
 
       const result = await this.searchService.searchUsersAndPosts(search);
-
-      console.log(result);
 
       res.status(200).json(result);
     } catch (error) {
