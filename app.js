cdd7eebbb57f8675ca815dac673af1403b6bace5
Jaa-van <127918643+Jaa-van@users.noreply.github.com>
2023-05-18 17:02:25 +0900

    Feat: Create socket for chat ver.1

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
-const http = require("http");
+const { Server } = require("http");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 require("express-async-errors");
-const app = express();
 const cors = require("cors");
+const app = express();
+
+const http = Server(app);
+const io = socketIo(http);
+
 const port = 3000;
+
+io.on("connection", (socket) => {
+  console.log("소켓이 연결되었습니다.");
+
+  socket.on("user join", (name, room) => {
+    (socket.name = name), (socket.room = room), socket.join(room);
+
+    io.to(socket.room).emit("update", {
+      type: "connect",
+      name: "administrator",
+      message: `${userId}님이 채팅창에 접속하셨습니다.`,
+    });
+  });
+
+  socket.on("chat message", (data) => {
+    (data.name = socket.name),
+      (data.room = socket.room),
+      socket.broadcast.to(socket.room).emit("chat message", data);
+  });
+});
 
 app.use(cors());
 
 const router = require("./routes");
-
 const errorHandler = require("./middlewares/error-handler");
 
 app.use(express.json());
@@ -17,6 +40,6 @@ app.use(cookieParser());
 app.use("/api", router);
 app.use(errorHandler);
 
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(port, "포트로 서버가 열렸어요!");
 });
